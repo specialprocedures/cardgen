e601cf0ef1fae5314d571d36c71a0044c08a574f
Ian Goodrich <goodrichian@gmail.com>
2025-04-25 14:03:05 +0400

    fix: update generator with real data and drop imgkit for now

--- card_generator.py
+++ card_generator.py
@@ -10,15 +10,15 @@ template = env.get_template("card_template.html")
 
 # Data for a single card (example)
 single_card_data = {
-    "id": "001",
-    "name": "Forest Guardian",
-    "type": "Creature",
-    "cost": 3,
-    "count": 1,
-    "effect": "When this card enters the battlefield, gain 5 life.",
-    "quote": '"Protector of the ancient woods."',
-    "source": "Elder Scrolls Series",
-    "image_url": "img/input/action1_bushtroops.jpg",  # Make sure this image exists or use a placeholder
+    "id": "action001",
+    "name": "Conscription",
+    "type": "Action",
+    "cost": 2,
+    "count": 4,
+    "effect": "You gain 6 units, place them on any territory you control.",
+    "quote": "If I were slightly younger and not employed here, I think it would be a fantastic experience to be on the front lines of helping this young democracy succeed.",
+    "source": "George W. Bush, 2008",
+    "image_url": "img/input/action1_bushtroops.jpg",
 }
 
 # Render the Jinja template for the single card
@@ -28,14 +28,16 @@ card_html = template.render(card=single_card_data)
 # We wrap the card HTML in a basic HTML structure
 full_html_string = f'<html><head><link rel="stylesheet" href="styles.css"></head><body>{card_html}</body></html>'
 
-# Use imgkit to convert HTML to png
-options = {
-    "format": "png",
-    "width": 300,
-    "height": 400,
-    "quality": 300,
-}
-
+# Write the HTML to a file
 with open("output_card.html", "w") as f:
     f.write(full_html_string)
+
+# Use imgkit to convert HTML to png
+# options = {
+#     "format": "png",
+#     "width": 300,
+#     "height": 400,
+#     "quality": 300,
+# }
+
 # imgkit.from_string(full_html_string, "output_card.png", options=options)
